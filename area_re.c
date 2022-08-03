8009f2118b5cd3ab851a1488883b0499fc4666f6
new1414 <110551758+new1414@users.noreply.github.com>
2022-08-03 20:46:26 +0530

    Add files via upload

---
+++ area_re.c
@@ -0,0 +1,17 @@
+//  Write a c program to calculate the area of a rectangle: using hardcoded inputs
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int l=3,b=5,A;
+
+     A=l*b;
+    printf("The Area of Rectangle is =%d ",A);
+    
+
+
+
+    return 0;
+}
+
